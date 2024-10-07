3bbf3a0cdb19dafbb8163b1e0d55d4de514134e8
Kirill <kirill.gurin20152016@yandex.ru>
2024-10-07 21:16:05 +0300

    Add-scripts

---
+++ Assets/Codebase/Car/SimpleCar.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class SimpleCar : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+
+    }
+}
